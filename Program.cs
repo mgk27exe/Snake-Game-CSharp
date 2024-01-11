4336d2fb5218c94d7837e80eeb2399b23dd5bbce
Muhammed Görkem Kılıç <91525759+mgk27exe@users.noreply.github.com>
2024-01-11 19:01:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using proje;
+using SnakeGame.ConsoleApp;
+using System.Drawing;
+
+
+Console.CursorVisible = false;
+
+int borderWidth = 50, borderHeigth = 20;
+int borderX = 20, borderY = 5;
+
+var message = "Press any key to start the game";
+
+Console.SetCursorPosition((Console.WindowWidth - message.Length) / 2, Console.CursorTop + 5);
+Console.WriteLine(message);
+
+Console.ReadKey();
+Console.Clear();
+
+Snake snake = new(snakeChar: 'O',
+                  snakePoint: new Point(5, 5),
+                  snakeInitialLength: 2,
+                  new Rectangle(borderX, borderY, borderWidth, borderHeigth));
+
+snake.SetSnakeChar('■');
+snake.SetInitialSnakeLength(5);
+snake.SetFoodChar('■');
+
+await snake.Run();
